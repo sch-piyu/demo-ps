8f2774bdfb5eb476eeca19f50685b996549e9ffb
scheweta_naik <scheweta_naik@GOD23771.persistent.co.in>
2016-01-22 16:12:02 +0530

    v1.0

---
+++ workspace/demops/src/com/psl/demoPackage/DemoClass.java
@@ -0,0 +1,14 @@
+package com.psl.demoPackage;
+
+public class DemoClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("scheweta");
+	}
+
+}
